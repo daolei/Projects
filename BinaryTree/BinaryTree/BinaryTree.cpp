24ff2a3658dc8263c8a59d2b5b3d7c5b028d9f95
daolei.su <daolei3935@163.com>
2015-11-12 16:32:16 +0800

    增加忽略特殊文件

--- BinaryTree/BinaryTree/BinaryTree.cpp
+++ BinaryTree/BinaryTree/BinaryTree.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include<iostream>
+#include<stack>
 using namespace std;
 class Node {
 public:
@@ -14,42 +15,119 @@ public:
 		this->value = value;
 	}
 };
-void preOrder(Node *head) {
+void preOrderRecur(Node *head) {
 	if (head == NULL)
 	{
 		return;
 	}
 	cout << head->value<<endl;
-	preOrder(head->left);
-	preOrder(head->right);
+	preOrderRecur(head->left);
+	preOrderRecur(head->right);
 }
-void InOrder(Node *head)
+void preOrderUnrecur(Node *head)
+{
+	if (head != NULL)
+	{
+		stack<Node*> stack;
+		stack.push(head);
+		while (!stack.empty())
+		{
+			head = stack.top();
+			cout << head->value<<endl;
+			stack.pop();
+			if (head->right != NULL)
+			{
+
+				stack.push(head->right);
+			}
+			if (head->left != NULL)
+			{
+				stack.push(head->left);
+			}
+		}
+	}
+}
+void InOrderRecur(Node *head)
 {
 	if (head == NULL)
 	{
 		return;
 	}
-	InOrder(head->left);
+	InOrderRecur(head->left);
 	cout << head->value<<endl;
-	InOrder(head->right);
+	InOrderRecur(head->right);
 
 }
-void posOrder(Node *head)
+void InOrderUnrecur(Node *head)
+{
+	if (head != NULL)
+	{
+		stack<Node*> stack;
+		while (!stack.empty() || head != NULL)
+		{
+			if (head != NULL)
+			{
+				stack.push(head);
+				head = head->left;
+			}
+			else {
+				head = stack.top();
+				stack.pop();
+				cout << head->value << endl;
+				head = head->right;
+			}
+		}
+	}
+}
+
+void posOrderRecur(Node *head)
 {
 	if (head == NULL)
 	{
 		return;
 	}
-	posOrder(head->left);
-	posOrder(head->right);
+	posOrderRecur(head->left);
+	posOrderRecur(head->right);
 	cout << head->value << endl;
+}
+void posOrderUnrecur(Node *head)
+{
+	if (head != NULL)
+	{
+		stack<Node*> s1;
+		stack<Node*> s2;
+		s1.push(head);
+		while (!s1.empty())
+		{
+			head = s1.top();
+			s1.pop();
+			s2.push(head);
+			if (head->left)
+			{
+				s1.push(head->left);
+			}
+			if (head->right)
+			{
+				s1.push(head->right);
+			}
+		}
+		while (!s2.empty())
+		{
+			head = s2.top();
+			s2.pop();
+			cout << head->value << endl;
+		}
+	}
 }
 int main()
 {
 	Node *head = new Node(1);
 	head->left = new Node(2);
 	head->right = new Node(3);
-	posOrder(head);
+	//posOrderRecur(head);
+	preOrderUnrecur(head);
+	InOrderUnrecur(head);
+	posOrderUnrecur(head);
     return 0;
 }
 
